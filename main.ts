f5a61580de75d6ad1cbc2d3ad1f6d4f7dd01ef81
leonidsharan <67856886+leonidsharan@users.noreply.github.com>
2020-07-05 16:24:43 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+let reading = 0
+pins.setPull(DigitalPin.P0, PinPullMode.PullDown)
+let prevReading = 1023
+basic.forever(function () {
+    reading = pins.analogReadPin(AnalogPin.P0)
+    led.plotBarGraph(
+    reading,
+    1023
+    )
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showNumber(reading)
+    }
+    if (reading - prevReading > 10) {
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(200)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+    prevReading = reading
+})
